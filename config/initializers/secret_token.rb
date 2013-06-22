a576eaa3b77a9319c8d2a312872458c65d48a843
Rob Freiburger <rob@robfreiburger.com>
2013-06-22 05:00:19 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bf4Webcon::Application.config.secret_key_base = '7c86f76173ca2f2b55bd1109d4305e584ac3758a2aa36af4cb09b87d393cb5bec6d0b9d1083cf53261a946fe9366482113b959feb6280d1e1895104835889bfa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bf4Webcon::Application.config.session_store :cookie_store, key: '_bf4_webcon_session'
