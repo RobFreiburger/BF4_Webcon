8235edd25cf941e908bb2e38a0826c0850e57b79
Rob Freiburger <rob@robfreiburger.com>
2013-07-22 11:29:15 -0700

    Fixes uninitalized ActiveRecord call.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,2 @@
 # Secret key is stored as a environment variable on Heroku.
-Bf4Webcon::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || ActiveRecord::KeyGenerator.generate_key(Random.new.rand(30..100))
+Bf4Webcon::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || Random.new.rand(30..100).to_s
