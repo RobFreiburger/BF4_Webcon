2875a922b4da3ea430d8e6488a1ecfea013a51e8
Rob Freiburger <rob@robfreiburger.com>
2013-07-31 14:59:47 -0700

    Updates unicorn configuration to Heroku standards.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,10 @@
-# default settings from Heroku
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
+# config/unicorn.rb
+worker_processes 3
+timeout 30
 preload_app true
 
 before_fork do |server, worker|
+
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -11,11 +12,12 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-end 
+end
 
 after_fork do |server, worker|
+
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
   end
 
   defined?(ActiveRecord::Base) and
