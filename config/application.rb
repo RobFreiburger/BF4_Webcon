bff57cdbab6adacd3f6dabd3db11ea8a1a63ed9e
Rob Freiburger <rob@robfreiburger.com>
2013-08-03 17:36:06 -0700

    Adds images to asset precompilation.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Bf4Webcon
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add images to asset precompile for Bootstrap
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
