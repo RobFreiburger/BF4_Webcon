127285b6e42d085611efa5871b16674318283bf0
Rob Freiburger <rob@robfreiburger.com>
2013-08-05 15:39:15 -0700

    Implements basic index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Bf4Webcon::Application.routes.draw do
-  get "openidtest/index"
-  get 'openidtest/start'
-  get 'openidtest/complete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'index#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ db/schema.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130801000157) do
+
+  create_table "servers", force: true do |t|
+    t.text     "name"
+    t.text     "description"
+    t.string   "battlelog_name"
+    t.string   "guid"
+    t.string   "ip_address"
+    t.integer  "rcon_port",      limit: 5
+    t.string   "rcon_password"
+    t.boolean  "allow_use",                default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "servers", ["name"], name: "index_servers_on_name", unique: true
+
+end
