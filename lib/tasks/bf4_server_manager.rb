b1e3e0b18be9ce349b7b59740052318d50e1007d
Rob Freiburger <rob@robfreiburger.com>
2013-10-06 14:16:22 -0700

    Adds example rake task for connecting Rails and the server management code.

---
+++ lib/tasks/bf4_server_manager.rb
@@ -0,0 +1,8 @@
+desc "BF4 Server Manager"
+task bf4_manager: :environment do
+	array = []
+	Server.all.each_index do |s, i|
+		array[i] = BFClient.new(host: s.ip_address, port: s.port_num, password: s.password)
+		array[i].manage_and_log_server
+	end
+end
