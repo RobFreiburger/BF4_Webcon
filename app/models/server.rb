37e5753cdbe9bb60fbb8b7461875a663d1f87bcc
Rob Freiburger <rob@robfreiburger.com>
2013-10-09 15:41:08 -0700

    Adds rcon_pw methods for handling encryption before storing in DB.

--- app/models/server.rb
+++ app/models/server.rb
@@ -7,4 +7,27 @@ class Server < ActiveRecord::Base
 		greater_than: 0,
 		less_than_or_equal_to: 65535
 	}
+
+	def rcon_pw
+		# Retrieve :rcon_pw and decrypt
+		decipher = OpenSSL::Cipher::AES.new(256, :CBC)
+		decipher.decrypt
+		decipher.iv = ENV['SERVER_PW_IV']
+		decipher.key = ENV['SERVER_PW_KEY']
+		encrypted = read_attribute(:rcon_pw)
+		decipher.update(encrypted) + decipher.final
+	end
+
+	def rcon_pw=(new_pw)
+		# Encrypt new_pw and store in DB
+		cipher = OpenSSL::Cipher::AES.new(256, :CBC)
+		cipher.encrypt
+		cipher.iv = ENV['SERVER_PW_IV']
+		cipher.key = ENV['SERVER_PW_KEY']
+		encrypted = cipher.update(new_pw) + cipher.final
+		write(:rcon_pw, encrypted)
+
+		# Return nothing
+		return
+	end
 end
